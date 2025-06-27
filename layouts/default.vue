ecca8558e3e988a658125068cc56e6918cfb58a8
guoqiangui <guoqiangui@gz-yibo.com>
2025-06-27 16:59:20 +0800

    feat: 引入tailwind 和 naive ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>导航栏</h1>
+    <h1
+      class="flex justify-between items-center border border-b-black p-[10px]"
+    >
+      <span>导航栏</span>
+      <div class="w-[50px] h-[50px] rounded-full bg-amber-700" />
+    </h1>
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     index page
-    <NuxtLink to="/detail/1">Detail</NuxtLink>
+    <NuxtLink to="/detail/1" class="text-blue-500">Detail</NuxtLink>
+    <n-button type="primary"> Primary </n-button>
   </div>
 </template>
